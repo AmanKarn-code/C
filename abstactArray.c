42ca25eda77a7d1d3cc6c0b338c48ac904faaa84
AmanKarn-code <amankarn.hzb1@gmail.com>
2023-12-13 11:20:36 +0530

    final

--- abstactArray.c
+++ abstactArray.c
@@ -8,21 +8,35 @@ struct abstactArray
     int *ptr;
 };
 
-void createArr(struct abstactArray *a,int tSize , int uSize){
-    (*a).totalSize=tSize;
-    (*a).usedSize=uSize;
-    (*a).ptr=(int *)malloc(tSize*sizeof(int)); 
+void createArr(struct abstactArray *a, int tSize, int uSize)
+{
+    (*a).totalSize = tSize;
+    (*a).usedSize = uSize;
+    (*a).ptr = (int *)malloc(tSize * sizeof(int));
 }
 
-void printArr(struct abstactArray *a){
+void printArr(struct abstactArray *a)
+{
+    printf("printing the array.\n");
     for (int i = 0; i < (*a).usedSize; i++)
     {
-        printf("[ %d ]",(a->ptr)[i]);
+        printf("[ %d ]", (a->ptr)[i]);
     }
-    
 }
-void main(){
+void setArr(struct abstactArray *a)
+{
+    printf("Taking the input of the array. \n");
+    for (int i = 0; i < (a->usedSize); i++)
+    {
+        printf("enter the no %d :- " , i+1);
+        scanf("%d",&(a->ptr)[i]);
+    }
+}
+void main()
+{
     struct abstactArray marks;
-    createArr(&marks,10,2);
+    createArr(&marks, 10, 2);
+    setArr(&marks);
     printArr(&marks);
+
 }
