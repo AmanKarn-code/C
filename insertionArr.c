7dc9a657f5a97c3f0ebb055c02608f02680028b5
AmanKarn-code <amankarn.hzb1@gmail.com>
2023-12-14 22:00:49 +0530

    second

--- insertionArr.c
+++ insertionArr.c
@@ -18,15 +18,33 @@ void displayArr(int arr[], int usedSize)
     }
     printf("]\n");
 }
-void insertEle(int arr[],int usedSize,int capacity,int index,int ele){
-    for(int i=usedSize-1;i>=index;i--){
-        arr[i+1]=arr[i];
+
+// // shifting all thye elements in the last
+// void insertEle(int arr[], int usedSize, int capacity, int index, int ele)
+// {
+//     for (int i = usedSize - 1; i >= index; i--)
+//     {
+//         arr[i + 1] = arr[i];
+//     }
+//     arr[index] = ele;
+// }
+
+void insertElement(int arr[], int usedSize, int capacity, int index, int ele)
+{
+    int a;
+    usedSize+=1;
+    arr[usedSize] = arr[index];
+    arr[index] = ele;
+    printf("%d", arr[usedSize]);
+    for(int i=usedSize;i>index;i--){
+        a=arr[index];
+        arr[index]=ele;
+        arr[usedSize+1]=a;
     }
-    arr[index]=ele;
 }
 int main()
 {
-    int capacity, usedSize,index=6,ele=5;
+    int capacity, usedSize, index = 3, ele = 5;
     printf("enter the capacity of the array : ");
     scanf("%d", &capacity);
     printf("enter the no of terms u want to add in the arr : ");
@@ -40,7 +58,10 @@ int main()
 
     createArr(arr, usedSize);
     displayArr(arr, usedSize);
-    insertEle(arr,usedSize,capacity,index,ele);
-    usedSize +=1;
+    // insertEle(arr,usedSize,capacity,index,ele);
+    // usedSize +=1;
+    // displayArr(arr, usedSize);
+    insertElement(arr, usedSize, capacity, index, ele);
+    usedSize += 1;
     displayArr(arr, usedSize);
 }
